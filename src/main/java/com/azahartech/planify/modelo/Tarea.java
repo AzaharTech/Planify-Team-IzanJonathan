cacafe5584bb6ae558aa193105687bb90721dcbc
Eskare <izanwey6@gmail.com>
2026-02-16 16:20:42 +0100

    Nuevo src

---
+++ src/main/java/com/azahartech/planify/modelo/Tarea.java
@@ -0,0 +1,37 @@
+package com.azahartech.planify.modelo;
+
+public class Tarea {
+    private static int contadorIds = 1; // Variable estática para generar IDs únicos
+    private int id;
+    private String descripcion;
+    private boolean completada;
+
+    public Tarea(String descripcion) {
+        this.id = contadorIds++;
+        this.descripcion = descripcion;
+        this.completada = false; // Por defecto, una nueva tarea no está completada
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public boolean isCompletada() {
+        return completada;
+    }
+
+    public void setCompletada(boolean completada) {
+        this.completada = completada;
+    }
+
+    // --- Método para imprimir la tarea de forma legible ---
+    @Override
+    public String toString() {
+        String estado = isCompletada() ? "[X]" : "[ ]";
+        return estado + " Tarea #" + id + ": " + descripcion;
+    }
+}
---
+++ src/main/java/com/azahartech/planify/modelo/Usuario.java
@@ -0,0 +1,35 @@
+package com.azahartech.planify.modelo;
+
+import java.util.Objects;
+public class Usuario {
+    String nombre;
+    String rol;
+
+    public Usuario(String nombre, String rol) {
+        this.nombre = nombre;
+        this.rol = rol;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getRol() {
+        return rol;
+    }
+
+    public void setRol(String rol) {
+        this.rol = rol;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        if (object == null || getClass() != object.getClass()) return false;
+        Usuario usuario = (Usuario) object;
+        return Objects.equals(nombre, usuario.nombre) && Objects.equals(rol, usuario.rol);
+    }
+}
